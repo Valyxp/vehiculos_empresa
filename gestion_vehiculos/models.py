ea86fb11512615aebbdfcbf9ab8983509d9bb0a8
Valery Maragaño <valery88_7@hotmail.com>
2024-11-24 20:05:11 -0300

    Definición de los modelos

--- gestion_vehiculos/models.py
+++ gestion_vehiculos/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Vehiculo(models.Model):
+    patente = models.CharField(max_length=6, primary_key=True)
+    marca = models.CharField(max_length=20)
+    modelo = models.CharField(max_length=20)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.marca} {self.modelo} ({self.patente})"
+
+
+class Chofer(models.Model):
+    rut = models.CharField(max_length=9, primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    activo = models.BooleanField(default=False)
+    creacion_registro = models.DateField(auto_now_add=True)
+    vehiculo = models.OneToOneField(
+        Vehiculo, on_delete=models.SET_NULL, null=True, blank=True, unique=True
+    )
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido} ({self.rut})"
+
+
+class RegistroContabilidad(models.Model):
+    id = models.AutoField(primary_key=True)
+    fecha_compra = models.DateField()
+    valor = models.FloatField()
+    vehiculo = models.OneToOneField(Vehiculo, on_delete=models.CASCADE, unique=True)
+
+    def __str__(self):
+        return f"Registro {self.id} - Vehículo {self.vehiculo.patente}"
